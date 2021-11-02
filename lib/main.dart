e7aa5fa7a5c5260a9e756681838fe844927133a7
tough man <limonraycse2020@gmail.com>
2021-11-02 17:21:20 +0600

    End

--- lib/main.dart
+++ lib/main.dart
@@ -10,81 +10,90 @@ class Amit extends StatefulWidget {
 }
 
 class _AmitState extends State<Amit> {
+  var question = [
+    {
+      'questionText': 'What is your name?',
+      'answer': ['A', 'B', 'C', 'D']
+    },
+    {
+      'questionText': 'What is your pet name?',
+      'answer': ['A', 'B', 'C', 'D']
+    },
+    {
+      'questionText': 'What is your car name?',
+      'answer': ['A', 'B', 'C', 'D']
+    },
+    {
+      'questionText': 'What is your animal name?',
+      'answer': ['A', 'B', 'C', 'D']
+    },
+    {
+      'questionText': 'What is your bird name?',
+      'answer': ['A', 'B', 'C', 'D']
+    },
+    {
+      'questionText': 'What is your phone name?',
+      'answer': ['A', 'B', 'C', 'D']
+    },
+    {
+      'questionText': 'What is your favorite watch name?',
+      'answer': ['A', 'B', 'C', 'D']
+    },
+    {
+      'questionText': 'What is your favorite dress name?',
+      'answer': ['A', 'B', 'C', 'D']
+    },
+    {
+      'questionText': 'What is your favorite shirt name?',
+      'answer': ['A', 'B', 'C', 'D']
+    },
+    {
+      'questionText': 'What is your favorite home name?',
+      'answer': ['A', 'B', 'C', 'D']
+    },
+    {
+      'questionText': 'What is your address?',
+      'answer': ['A', 'B', 'C', 'D']
+    },
+    {
+      'questionText': 'What is your favorite pen name?',
+      'answer': ['A', 'B', 'C', 'D']
+    },
+  ];
   var _answerIndex = 0;
   void _answerQuestion() {
     setState(() {
       _answerIndex = _answerIndex + 1;
     });
+    print(_answerIndex);
+
+    if (_answerIndex < question.length) {
+      print('you have more questions');
+    } else {
+      print('you are done!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var question = [
-      {
-        'questionText': 'What is your name?',
-        'answer': ['A', 'B', 'C', 'D']
-      },
-      {
-        'questionText': 'What is your pet name?',
-        'answer': ['A', 'B', 'C', 'D']
-      },
-      {
-        'questionText': 'What is your car name?',
-        'answer': ['A', 'B', 'C', 'D']
-      },
-      {
-        'questionText': 'What is your animal name?',
-        'answer': ['A', 'B', 'C', 'D']
-      },
-      {
-        'questionText': 'What is your bird name?',
-        'answer': ['A', 'B', 'C', 'D']
-      },
-      {
-        'questionText': 'What is your phone name?',
-        'answer': ['A', 'B', 'C', 'D']
-      },
-      {
-        'questionText': 'What is your favorite watch name?',
-        'answer': ['A', 'B', 'C', 'D']
-      },
-      {
-        'questionText': 'What is your favorite dress name?',
-        'answer': ['A', 'B', 'C', 'D']
-      },
-      {
-        'questionText': 'What is your favorite shirt name?',
-        'answer': ['A', 'B', 'C', 'D']
-      },
-      {
-        'questionText': 'What is your favorite home name?',
-        'answer': ['A', 'B', 'C', 'D']
-      },
-      {
-        'questionText': 'What is your address?',
-        'answer': ['A', 'B', 'C', 'D']
-      },
-      {
-        'questionText': 'What is your favorite pen name?',
-        'answer': ['A', 'B', 'C', 'D']
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Amit Quiz'),
+          title: Center(child: Text('Amit Quiz')),
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Questions(question[_answerIndex]['questionText'] as String),
-              ...(question[_answerIndex]['answer'] as List<String>)
-                  .map((answer) {
-                return Answers(_answerQuestion, answer);
-              }).toList(),
-            ],
-          ),
-        ),
+        body: _answerIndex < question.length
+            ? Column(
+                children: [
+                  Questions(question[_answerIndex]['questionText'] as String),
+                  ...(question[_answerIndex]['answer'] as List<String>)
+                      .map((answer) {
+                    return Answers(_answerQuestion, answer);
+                  }),
+                ],
+              )
+            : Center(
+                child: Text('This Quiz is completed'),
+              ),
       ),
     );
   }
